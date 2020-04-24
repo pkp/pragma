f05209c5a606bf6c2b2be43073c468e0b68274bd
Vitaliy Bezsheiko <vitaliybezsh@gmail.com>
2020-04-24 12:35:46 +0300

    Copyright date update

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
-	
+
 	/**
 	 * @file plugins/themes/pragma/index.php
 	 *
-	 * Copyright (c) 2014-2019 Simon Fraser University
-	 * Copyright (c) 2003-2019 John Willinsky
+	 * Copyright (c) 2014-2020 Simon Fraser University
+	 * Copyright (c) 2003-2020 John Willinsky
 	 * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
 	 *
 	 * @ingroup plugins_themes_pragma
 	 * @brief Wrapper for Pragma theme plugin.
 	 *
 	 */
-	
+
 	require_once('PragmaThemePlugin.inc.php');
-	
+
 	return new PragmaThemePlugin();
--- templates/frontend/pages/message.tpl
+++ templates/frontend/pages/message.tpl
@@ -1,7 +1,7 @@
 {**
  * templates/frontend/pages/message.tpl
  *
- * Copyright (c) 2014-2019 Simon Fraser University
+ * Copyright (c) 2014-2020 Simon Fraser University
  * Copyright (c) 2000-2019 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
