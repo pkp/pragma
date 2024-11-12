bcb5b3e01efedf9ebc277440e803701ac0b44c00
Kaitlin Newson <kaitlin.newson@gmail.com>
2024-11-12 15:11:49 -0400

    fix tests

--- .github/actions/tests.sh
+++ .github/actions/tests.sh
@@ -4,5 +4,4 @@ set -e
 
 npx cypress run --spec "cypress/tests/data/10-ApplicationSetup/*.cy.js,cypress/tests/data/60-content/VkarbasizaedSubmission.cy.js"
 
-npx cypress run --config specPattern=plugins/themes/pragma/cypress/tests/functional
-
+npx cypress run --headless --browser chrome  --config '{"specPattern":["plugins/themes/pragma/cypress/tests/functional/*.cy.js"]}'
