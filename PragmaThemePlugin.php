4c8568f008f20385e342467317ddd112490f8d7b
Alec Smecher <alec@smecher.bc.ca>
2026-03-27 18:02:24 -0700

    #13 Do not assume a current issue

--- PragmaThemePlugin.php
+++ PragmaThemePlugin.php
@@ -255,7 +255,7 @@ class PragmaThemePlugin extends ThemePlugin
             $currentIssue = Repo::issue()->getCurrent($context->getId());
             if ($navigationMenuItem->getType() === NavigationMenuService::NMI_TYPE_CURRENT) {
                 $issue = $smarty->getTemplateVars('issue');
-                if ($issue && ($issue->getId() === $currentIssue->getId()) && $currentPage == "issue") {
+                if ($issue && $currentIssue && ($issue->getId() === $currentIssue->getId()) && $currentPage == "issue") {
                     return $activeMarker;
                 }
             }
